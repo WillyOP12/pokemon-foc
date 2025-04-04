ba830869511aae331ae20c499fa98c7b8d1d324f
WillyOP12 <guiorpa@gmail.com>
2025-04-04 18:26:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function login() {
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
+document.getElementById("loginBtn").addEventListener("click", login);
 
-  if (username === "WillyOP12" && password === "guillem@12") {
-    // Admin
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "WillyOP12" && pass === "guillem@12") {
     localStorage.setItem("isAdmin", "true");
-    localStorage.setItem("username", username);
+    localStorage.setItem("username", user);
     window.location.href = "admin.html";
-  } else if (username && password) {
-    // Usuari normal
+  } else if (user && pass) {
     localStorage.setItem("isAdmin", "false");
-    localStorage.setItem("username", username);
+    localStorage.setItem("username", user);
     window.location.href = "home.html";
   } else {
-    alert("Introdueix usuari i contrasenya!");
+    alert("Falten dades!");
   }
 }
